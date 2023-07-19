6eeb4743d116df00a0cdda218cd1f6448ff31755
Olle Axelsson <soa.96@hotmail.com>
2023-07-19 21:03:18 +0200

    Initial rls

---
+++ src/main.rs
@@ -0,0 +1,70 @@
+use std::env;
+use std::path::PathBuf;
+use std::fs::DirEntry;
+
+use clap::Parser;
+use env_logger;
+use log::{trace, warn, error, log_enabled, Level};
+
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct LsArgs {
+    /// Directory to list
+    name: Option<String>,
+
+    /// List files
+    #[arg(short, long)]
+    list: bool,
+}
+
+fn main() {
+    env_logger::init();
+    let args = LsArgs::parse();
+    trace!("Starting rsls");
+    trace!("list: {}", args.list);
+
+    // current dir or sub dir
+    let path_buf = match args.name {
+        Some(name) => PathBuf::from(name),
+        None => env::current_dir().expect("Failed to read current dir"),
+    };
+    trace!("list dir: {:?}", path_buf.file_name().expect("Could not name"));
+
+    if !path_buf.is_dir() {
+        error!("Not a directory");
+    }
+
+    let dir_entry = path_buf.read_dir()
+        .expect("Failed to read directory")
+        .filter_map(|e| e.ok() )
+        .collect();
+
+    match args.list {
+        true => list(&dir_entry),
+        false => list_info(&dir_entry),
+    }
+}
+
+fn list (entries: &Vec<DirEntry>) {
+    for e in entries.into_iter() {
+        if let Some(name) = e.path()
+            .file_name()
+            .expect("failed to read file name")
+            .to_str() {
+            println!("{name}");
+        }
+    }
+}
+
+fn list_info(entries: &Vec<DirEntry>) {
+    for e in entries.into_iter() {
+        if let Some(name) = e.path()
+            .file_name()
+            .expect("failed to read file name")
+            .to_str() {
+            print!("{name}\t");
+        }
+    }
+}
+
