f39014da411e3afb1da1142fd4da5f0b7438d507
Olle Axelsson <soa.96@hotmail.com>
2023-07-21 17:43:14 +0200

    refactor and lookup name

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
+use clap::Parser;
+use log::{error, trace};
 use std::env;
 use std::fs::DirEntry;
 use std::os::unix::fs::MetadataExt;
 use std::path::PathBuf;
-
-use clap::Parser;
-use log::{error, trace};
+use users::{Users, UsersCache};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -19,6 +19,14 @@ struct LsArgs {
     /// List all files
     #[arg(short, long)]
     all: bool,
+}
+
+struct ListParams {
+    modes: String,
+    usr: String,
+    grp: String,
+    size: u64,
+    name: String,
 }
 
 fn main() {
@@ -89,33 +97,75 @@ fn list(entries: &Vec<DirEntry>) {
 fn list_info(entries: &Vec<DirEntry>) {
     println!("Mode\t\t user\t group\t size\t name");
     for e in entries.iter() {
-        if let Ok(metadata) = e.metadata() {
-            // TODO: Change this
-            if let Some(name) = e.path().file_name() {
-                let name = name.to_str().expect("OS String failed to be converted");
-                let d = if metadata.is_dir() { "d" } else { "-" };
-                let size = metadata.size();
-                let mode = metadata.mode();
-                let usr = metadata.uid(); // TODO: Convert to text
-                let grp = metadata.gid(); // TODO: Convert to text
+        let list_params = parse_dir_entry(&e).unwrap();
 
-                let ue = if mode & 0o100 > 0 { "x" } else { "-" };
-                let ur = if mode & 0o200 > 0 { "r" } else { "-" };
-                let uw = if mode & 0o400 > 0 { "w" } else { "-" };
-
-                let ge = if mode & 0o010 > 0 { "x" } else { "-" };
-                let gr = if mode & 0o020 > 0 { "r" } else { "-" };
-                let gw = if mode & 0o040 > 0 { "w" } else { "-" };
-
-                let ae = if mode & 0o001 > 0 { "x" } else { "-" };
-                let ar = if mode & 0o002 > 0 { "r" } else { "-" };
-                let aw = if mode & 0o004 > 0 { "w" } else { "-" };
-
-                println!(
-                    "{d}{ur}{uw}{ue}{gr}{gw}{ge}{ar}{aw}{ae}\t {:?}\t {:?}\t {:?}\t {}",
-                    usr, grp, size, name
-                );
-            }
-        }
+        println!(
+            "{}\t {}\t {}\t {:?}\t {}",
+            list_params.modes, list_params.usr, list_params.grp, list_params.size, list_params.name
+        );
     }
+}
+
+fn parse_dir_entry(e: &DirEntry) -> Result<ListParams, String> {
+    let metadata = e.metadata().map_err(|_| String::from("Metadata"))?;
+
+    /* Get name of file */
+    let path = e.path();
+    let name = path.file_name().ok_or("name")?;
+    let name = name.to_str().ok_or("Name")?;
+
+    /* Get permission of file */
+    let d = if metadata.is_dir() { "d" } else { "-" };
+    let mode = metadata.mode();
+
+    /* A bit ugly, but converting permission to letter */
+    let ue = if mode & 0o100 > 0 { "x" } else { "-" };
+    let ur = if mode & 0o200 > 0 { "r" } else { "-" };
+    let uw = if mode & 0o400 > 0 { "w" } else { "-" };
+
+    let ge = if mode & 0o010 > 0 { "x" } else { "-" };
+    let gr = if mode & 0o020 > 0 { "r" } else { "-" };
+    let gw = if mode & 0o040 > 0 { "w" } else { "-" };
+
+    let ae = if mode & 0o001 > 0 { "x" } else { "-" };
+    let ar = if mode & 0o002 > 0 { "r" } else { "-" };
+    let aw = if mode & 0o004 > 0 { "w" } else { "-" };
+
+    let mut modes = String::new();
+    modes.push_str(d);
+    modes.push_str(ue);
+    modes.push_str(ur);
+    modes.push_str(uw);
+    modes.push_str(ge);
+    modes.push_str(gr);
+    modes.push_str(gw);
+    modes.push_str(ae);
+    modes.push_str(ar);
+    modes.push_str(aw);
+
+    /* Get user and group of file */
+    let usr = metadata.uid();
+    let grp = metadata.gid();
+
+    /* Convert uid and gid to name */
+    let cache = UsersCache::new();
+
+    let usr = cache.get_user_by_uid(usr).ok_or("Owner")?;
+    let usr = usr.name().to_str().ok_or(String::from("Owner"))?;
+
+    let grp = cache.get_user_by_uid(grp).ok_or("Group")?;
+    let grp = grp.name().to_str().ok_or(String::from("group"))?;
+
+    /* Get size of file */
+    let size = metadata.size();
+
+    let list_params = ListParams {
+        modes,
+        usr: String::from(usr),
+        grp: String::from(grp),
+        size,
+        name: String::from(name),
+    };
+
+    Ok(list_params)
 }
